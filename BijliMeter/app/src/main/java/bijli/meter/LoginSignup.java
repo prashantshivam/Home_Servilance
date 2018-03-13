b8812fedd11369cce120d0d17af9a77d0349fecd
rajeeev001 <rajeeev001@gmail.com>
2018-03-13 11:23:47 +0530

    code

---
+++ BijliMeter/app/src/main/java/bijli/meter/LoginSignup.java
@@ -0,0 +1,13 @@
+package bijli.meter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginSignup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signup);
+    }
+}
